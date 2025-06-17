a4d25d0a1130a6917e0e81877f44b1f66dc012e3
Javion <javionlog@163.com>
2025-06-17 18:13:22 +0800

    wip: drizzle add schema

--- apps/api/src/modules/auth/lib/better-auth/index.ts
+++ apps/api/src/modules/auth/lib/better-auth/index.ts
@@ -3,10 +3,11 @@ import { drizzle } from 'drizzle-orm/neon-http'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { betterAuth } from 'better-auth'
 import { openAPI, username } from 'better-auth/plugins'
+import * as schema from '../../../../db/schemas/auth'
 
 const { DATABASE_URL, BETTER_AUTH_URL, BETTER_AUTH_SECRET } = process.env
 const sql = neon(DATABASE_URL)
-const db = drizzle(sql)
+const db = drizzle(sql, { schema })
 
 export const auth: ReturnType<typeof betterAuth> = betterAuth({
   basePath: '/auth',
@@ -16,5 +17,9 @@ export const auth: ReturnType<typeof betterAuth> = betterAuth({
     enabled: true
   },
   database: drizzleAdapter(db, { provider: 'pg' }),
+  trustedOrigins() {
+    const url = BETTER_AUTH_URL.replace(/^https?:\/\//, '')
+    return [`https://${url}`, `http://${url}`]
+  },
   plugins: [openAPI(), username()]
 })
