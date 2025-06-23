69f8b02c063d55aa1849ce91b1794566e7de9bd5
Javion <javionlog@163.com>
2025-06-23 10:51:51 +0800

    wip: optimize env

--- apps/api/src/modules/auth/index.ts
+++ apps/api/src/modules/auth/index.ts
@@ -4,9 +4,23 @@ import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { betterAuth } from 'better-auth'
 import { openAPI, username } from 'better-auth/plugins'
 import * as schema from '../../db/schemas/auth'
+import { env } from 'cloudflare:workers'
 
-const { DATABASE_URL, BETTER_AUTH_URL, BETTER_AUTH_SECRET, BETTER_AUTH_TRUSTED_ORIGINS } =
-  process.env
+const getEnv = () => {
+  const DATABASE_URL = process.env.DATABASE_URL ?? env.DATABASE_URL
+  const BETTER_AUTH_URL = process.env.BETTER_AUTH_URL ?? env.BETTER_AUTH_URL
+  const BETTER_AUTH_SECRET = process.env.BETTER_AUTH_SECRET ?? env.BETTER_AUTH_SECRET
+  const BETTER_AUTH_TRUSTED_ORIGINS =
+    process.env.BETTER_AUTH_TRUSTED_ORIGINS ?? env.BETTER_AUTH_TRUSTED_ORIGINS
+  return {
+    DATABASE_URL,
+    BETTER_AUTH_URL,
+    BETTER_AUTH_SECRET,
+    BETTER_AUTH_TRUSTED_ORIGINS
+  }
+}
+
+const { DATABASE_URL, BETTER_AUTH_URL, BETTER_AUTH_SECRET, BETTER_AUTH_TRUSTED_ORIGINS } = getEnv()
 const sql = neon(DATABASE_URL)
 const db = drizzle(sql, { schema })
 
