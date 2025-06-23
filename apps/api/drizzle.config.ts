813f78ce42494245c54b2e8d3d1fed1880fe559d
Javion <javionlog@163.com>
2025-06-23 11:56:12 +0800

    wip: optimize env

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'drizzle-kit'
+import { env } from 'cloudflare:workers'
 
 export default defineConfig({
   out: './src/db/migrations',
   schema: './src/db/schemas/**.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL
+    url: env.DATABASE_URL
   }
 })
