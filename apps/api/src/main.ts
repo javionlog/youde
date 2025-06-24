86c23a0eba0b883dc42bbe794f18d2b11b8682c9
Javion <javionlog@163.com>
2025-06-24 14:07:22 +0800

    wip: optimize env

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,8 +3,6 @@ import { auth } from './modules/auth'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
-app.get('/hello', c => c.json({ msg: 'Hello' }))
-
 app.all('/auth/*', async c => {
   const response = await auth(c.env).handler(c.req.raw)
   if (response.status === 404) {
