26c67ce4edac6b0057f48f77efe5e2f310618ede
Javion <javionlog@163.com>
2025-08-13 11:22:31 +0800

    wip: add postgresql

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -1,10 +1,12 @@
-import { neon } from '@neondatabase/serverless'
-import { drizzle } from 'drizzle-orm/neon-http'
+import { drizzle } from 'drizzle-orm/postgres-js'
 
 import * as schema from './schemas/auth'
 
 const { DATABASE_URL } = process.env
 
-const sql = neon(DATABASE_URL)
-
-export const db = drizzle(sql, { schema })
+export const db = drizzle({
+  connection: {
+    url: DATABASE_URL
+  },
+  schema
+})
