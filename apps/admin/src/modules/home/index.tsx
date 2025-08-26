7f1e491e541511deab726840ea39c70d5431d4a8
Javion <javionlog@163.com>
2025-08-26 09:55:32 +0800

    wip: upgrade tdesign-react

--- apps/admin/src/modules/home/index.tsx
+++ apps/admin/src/modules/home/index.tsx
@@ -7,23 +7,19 @@ export default () => {
   const { setResourceTree, setResourceInited } = useResourceStore()
   const user = useUserStore(state => state.user)
 
-  const handleSignout = () => {
-    postAuthSignOut().then(() => {
-      navigate('/sign-in')
-      // TODO: router no redirect
-      setUser(null)
-      setResourceTree([])
-      setResourceInited(false)
-    })
+  const handleSignout = async () => {
+    await postAuthSignOut()
+    navigate('/sign-in')
+    setUser(null)
+    setResourceTree([])
+    setResourceInited(false)
   }
   return (
-    <>
-      <div>
-        <div>姓名: {user?.name}</div>
-        <div>用户名: {user?.username}</div>
-        <div>用户邮箱: {user?.email}</div>
-        <Button onClick={handleSignout}>登出</Button>
-      </div>
-    </>
+    <div>
+      <div>姓名: {user?.name}</div>
+      <div>用户名: {user?.username}</div>
+      <div>用户邮箱: {user?.email}</div>
+      <Button onClick={handleSignout}>登出</Button>
+    </div>
   )
 }
