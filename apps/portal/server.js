c60e4c087836401d7ce46056109f60b4714c9d74
Javion <javionlog@163.com>
2025-10-28 11:46:12 +0800

    wip: add http-proxy-middleware

--- apps/portal/server.js
+++ apps/portal/server.js
@@ -1,6 +1,7 @@
 import compression from 'compression'
 import { consola } from 'consola'
 import express from 'express'
+import { createProxyMiddleware } from 'http-proxy-middleware'
 import morgan from 'morgan'
 
 const serverEntry = './dist/server/index.js'
@@ -12,6 +13,16 @@ app.use(compression())
 app.disable('x-powered-by')
 
 app.use('/assets', express.static('dist/client/assets', { immutable: true, maxAge: '1y' }))
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: `http://${process.env.VITE_API_HOST_NAME}:${process.env.VITE_API_HOST_PORT}`,
+    changeOrigin: true,
+    pathRewrite: path => {
+      return path.replace(/^\/api/, '')
+    }
+  })
+)
 app.use(morgan('tiny'))
 app.use(express.static('dist/client', { maxAge: '1h' }))
 app.use(await import(serverEntry).then(mod => mod.app))
