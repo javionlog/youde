24962ac2079f94ae5f40d9179d9754ae25fc0896
Javion <javionlog@163.com>
2026-01-08 16:53:45 +0800

    wip: develop portal home page

--- apps/portal/src/global/layouts/index.tsx
+++ apps/portal/src/global/layouts/index.tsx
@@ -1,31 +1,7 @@
-import type { TabBarProps } from 'tdesign-mobile-react'
-import { layoutRoutes } from '@/routes'
-
 export default () => {
-  const location = useLocation()
-  const navigate = useNavigate()
-
-  const [tabValue, setTabValue] = useState(location.pathname)
-
-  const onTabChange: TabBarProps['onChange'] = value => {
-    if (typeof value === 'string') {
-      setTabValue(value)
-      navigate(value)
-    }
-  }
-
   return (
-    <div className='app-layout mx-auto h-dvh max-w-lg pb-[56px]'>
+    <div className='app-layout mx-auto h-dvh max-w-lg'>
       <Outlet />
-      <TabBar value={tabValue} onChange={onTabChange}>
-        {layoutRoutes.map(item => {
-          return (
-            <TabBarItem key={item.path} icon={item.icon} value={item.path}>
-              {item.name}
-            </TabBarItem>
-          )
-        })}
-      </TabBar>
     </div>
   )
 }
--- apps/portal/src/modules/home/_index.tsx
+++ apps/portal/src/modules/home/_index.tsx
@@ -1,7 +1,72 @@
+import type { TdListProps } from 'tdesign-mobile-react'
+import type { PostPortalTreasureListResponse } from '@/global/api'
+import { postPortalTreasureList } from '@/global/api'
+import type { Route } from './+types/_index'
+
 export const meta = () => {
-  return [{ title: '首页' }, { name: 'description', content: '首页' }]
+  return [{ title: 'Youde Portal' }, { name: 'description', content: 'Welcome to Youde Portal' }]
 }
 
-export default () => {
-  return <div>首页</div>
+export const loader = async () => {
+  const result = await postPortalTreasureList({ body: { page: 1, pageSize: 10 } })
+  return result
+}
+
+export default ({ loaderData }: Route.ComponentProps) => {
+  const [loadingText, setLoadingText] = useState('')
+  const [refreshing, setRefreshing] = useState(false)
+  const page = useRef(1)
+  const total = useRef(loaderData.data?.total ?? 0)
+  const records = useRef<PostPortalTreasureListResponse['records']>(loaderData.data?.records ?? [])
+
+  const fetchRecords = async (isRefresh = false) => {
+    if ((records.current.length >= total.current && !isRefresh) || refreshing) {
+      return
+    }
+    setLoadingText('loading')
+    if (isRefresh) {
+      page.current = 1
+    } else {
+      page.current += 1
+    }
+    const res = await postPortalTreasureList({
+      body: { page: page.current, pageSize: 10 }
+    }).then(r => r.data!)
+    if (isRefresh) {
+      records.current = res.records
+    } else {
+      records.current = [...records.current, ...res.records]
+    }
+    total.current = res.total
+    setLoadingText('')
+    setRefreshing(false)
+  }
+
+  const onRefresh = () => {
+    setRefreshing(true)
+    fetchRecords(true)
+  }
+
+  const onScroll: TdListProps['onScroll'] = bottomDistance => {
+    if (bottomDistance < 50) {
+      fetchRecords()
+    }
+  }
+
+  return (
+    <PullDownRefresh
+      value={refreshing}
+      onChange={setRefreshing}
+      onRefresh={onRefresh}
+      className='overflow-auto!'
+    >
+      <List asyncLoading={loadingText} onScroll={onScroll}>
+        {records.current.map(item => {
+          return (
+            <Cell key={item.id} align='middle' title={item.title} description={item.description} />
+          )
+        })}
+      </List>
+    </PullDownRefresh>
+  )
 }
