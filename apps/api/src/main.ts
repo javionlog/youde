f52fa6ba2496dd5c60c6bcb707f56ceb7f9da783
Javion <javionlog@163.com>
2026-05-12 13:47:41 +0800

    wip: add listen

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,8 @@
 import { Elysia } from 'elysia'
 import { appController } from './app-controller'
 
-const app = new Elysia().use(appController)
+const app = new Elysia().use(appController).listen(3000)
 
 export type App = typeof app
 
-export default {
-  ...app,
-  port: 3000
-}
+export default app
