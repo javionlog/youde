98c511e6244c22e0ba443da36a1ebef17fd12395
Javion <javionlog@163.com>
2026-05-13 15:52:31 +0800

    wip: optimize Dockerfile

--- apps/api/bun.build.ts
+++ apps/api/bun.build.ts
@@ -2,9 +2,6 @@ import { cp, rm } from 'node:fs/promises'
 import { join } from 'node:path'
 
 const baseDir = import.meta.dir
-/* biome ignore */
-console.log('baseDir:', baseDir)
-console.log('resolved:', import.meta.resolve('@youde/shared'))
 
 await rm(join(baseDir, 'dist'), { recursive: true, force: true })
 
